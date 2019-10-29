80c9e730f453f105dc214374a8dfa713cf8943af
VladKit <vlad.chertykov@yandex.ru>
2019-10-29 20:34:09 +0500

    Сделал задание со светофором.

--- 6_lesson/lesson_6_task_1.py
+++ 6_lesson/lesson_6_task_1.py
@@ -1,19 +1,23 @@
+from time import *
+
 class TrafficLight:
 
-    from threading import Timer
-    from time import sleep
+    __color = ['Зеленый', 'Желтый', 'Красный']
+    stage = 1
 
-    def hello():
-        print("Hello user!")
-
-    def bye():
-        sleep(10)
-        print("Bye...bye...")
-
-    while 1:
-        timer = Timer(2, hello)
-        timer.start()
-        bye()
+    def running(self):
+        i = 0
+        while True:
+            if TrafficLight.stage == 1:
+                if i == 3: i = 0
+                print('Свктафор мигает')
+                TrafficLight.stage = 2
+                sleep(2)
+            else:
+                print(TrafficLight.__color[i])
+                TrafficLight.stage = 1
+                i += 1
+                sleep(7)
 
 work_TrafficLight = TrafficLight()
 work_TrafficLight.running()
